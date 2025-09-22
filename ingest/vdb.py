c94b69444ae63fba159a05d05f50b42e8a66bdf9
mttgermano <mttgermano@proton.me>
2025-09-22 20:47:32 -0300

    RAG working

--- ingest/vdb.py
+++ ingest/vdb.py
@@ -5,10 +5,12 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_core.documents import Document
+from time import time
 
 
 def start_vdb():
     df = pd.read_csv("./dataset.csv")
+    print("[*] CSV loaded")
     
     docs = [
         Document(
@@ -24,13 +26,17 @@ def start_vdb():
     # Vector Store com FAISS e embeddings
     embedding_model = HuggingFaceEmbeddings(model_name="thenlper/gte-small")
     vectorstore = FAISS.from_documents(documents=splits, embedding=embedding_model)
+    print("[*] VDB loaded")
     
     retriever = vectorstore.as_retriever(search_kwargs={'k': 4})
     return retriever
 
+t0 = time()
 r = start_vdb()
+t1 = time()
+print(t0-t1)
 test_question = "What is the use of P85-Ab?"
-retrieved_docs = retriever.invoke(test_question)
+retrieved_docs = r.invoke(test_question)
 
 for doc in retrieved_docs:
     print(f"Page Content: {doc.page_content[:150]}...")
